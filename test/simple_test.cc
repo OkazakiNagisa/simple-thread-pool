3764b7aee3ab479994c6aec878a32359db1c3dde
nakido <nakidosama@outlook.com>
2024-04-06 01:46:10 +0800

    build: write tests

--- test/simple_test.cc
+++ test/simple_test.cc
@@ -3,15 +3,25 @@
 
 using stp::ThreadPool;
 
-TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]")
+TEST_CASE("Evaluate task future correctness", "[stp]")
 {
-    REQUIRE(Factorial(0) == 1);
+    ThreadPool tp;
+    auto f = [](int a) {
+        int ret = 0;
+        for (int i = 0; i < a; i++)
+        {
+            ret += 10 * i;
+        }
+        return ret;
+    };
+    auto future = tp.EnqueueTask(f, 1000000);
+
+    REQUIRE(future.get() == f(1000000));
+    tp.WaitAll();
 }
 
-TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]")
+TEST_CASE("Evaluate multi tasks", "[stp]")
 {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+    ThreadPool tp;
+    tp.WaitAll();
 }
